437c4d8bd58f110c21a70c374915f2154067d5b7
darazolt@gmail.com <https://github.com/Zodatheonly/vuexpractice.git>
2022-03-18 15:59:01 +0100

    teszt

--- HelloKotlin/src/main/kotlin/FancyInfoProvider.kt
+++ HelloKotlin/src/main/kotlin/FancyInfoProvider.kt
@@ -8,6 +8,6 @@ class FancyInfoProvider : BasicInfoProvider() {
 
     override fun printInfo(person: Person) {
         super.printInfo(person)
-        println("FancyInfo")
+        println("FancyInfoo")
     }
 }
